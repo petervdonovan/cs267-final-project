50181da765f1ebe20c796d717340c9ecdf0ff2a5
Peter Donovan <peterdonovan@berkeley.edu>
2023-04-23 14:12:52 -0700

    Add test for reduce-shmem.

--- flexpret/idot/pyt.py
+++ flexpret/idot/pyt.py
@@ -5,9 +5,10 @@ import subprocess
 
 for path in os.listdir("test"):
     if path.endswith(".rvg"):
-        output = subprocess.check_output(["rbu", "-b", "test.rbu", "test/0.rvg"]).decode()
+        subprocess.check_output(["rbu", "-b", "test.rbu", os.path.join("test", path)])
+        subprocess.check_output(["riscv-compile.sh", "8", "ispm", os.path.join("src-gen", path.replace(".rvg", ".s"))])
         output = subprocess.run(["fp-emu"], stderr=subprocess.STDOUT, check=False, stdout=subprocess.PIPE).stdout.decode()
-        good_path = os.path.join("test/good/", os.path.basename(path))
+        good_path = os.path.join("test/good/", os.path.basename(path).replace(".rvg", ".out"))
         if os.path.exists(good_path):
             with open(good_path, "r") as f:
                 good = f.read()
@@ -16,6 +17,6 @@ for path in os.listdir("test"):
             if good != output:
                 print(subprocess.check_output(["git", "diff", "--", good_path]).decode())
         else:
-            os.makedirs(os.path.abspath(os.path.join(good_path, os.path.pardir)))
+            os.makedirs(os.path.abspath(os.path.join(good_path, os.path.pardir)), exist_ok=True)
             with open(good_path, "a") as f:
                 f.write(output)
